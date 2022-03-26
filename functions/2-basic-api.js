2e72c6429334ef9bf67778b54ffe13d64de60f04
KIRILL F <tiltlessman@gmail.com>
2022-03-26 16:40:54 +0300

    small changes

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,6 +1,14 @@
 const items = require('../assets/data')
 
 exports.handler = async (event, context, cb) => {
+  const { id } = event.queryStringParameters
+  const ids = items.filter((item) => item.id !== id)
+  if (ids) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(ids),
+    }
+  }
   return {
     headers: {
       'Access-Control-Allow-Origin': '*',
