425b2a411060ce99a85119dc7f18d6fca387db20
KIRILL F <tiltlessman@gmail.com>
2022-04-06 18:31:31 +0300

    fix single product

--- functions/single_product.js
+++ functions/single_product.js
@@ -3,14 +3,16 @@ const items = require('../assets/data')
 exports.handler = async (event, context, cb) => {
   const { id } = event.queryStringParameters
   const ids = items.filter((item) => item.id === id)
+  const item = ids[0]
+  console.log(item)
 
-  if (ids) {
+  if (item) {
     return {
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
       statusCode: 200,
-      body: JSON.stringify(ids),
+      body: JSON.stringify(item),
     }
   }
 }
